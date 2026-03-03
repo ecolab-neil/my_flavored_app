a3ff676bb8744fefc3199addaa8345604602131c
ecolab-neil <neil.warner@ecolab.com>
2026-03-02 18:16:29 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/my/flavored/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.verygoodcore.my_flavored_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:my_flavored_app/app/app.dart';
+import 'package:my_flavored_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
